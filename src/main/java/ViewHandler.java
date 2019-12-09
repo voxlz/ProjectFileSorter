465f26df2d5013b63a65d7ab70e38839b3c32f0a
Torben Nordtorp <torben.nordtorp@icloud.com>
2019-12-09 18:17:45 +0100

    Updated GUI and fixed sorting algorithm.
    Signed-off-by: Torben Nordtorp <torben.nordtorp@icloud.com>

--- src/main/java/ViewHandler.java
+++ src/main/java/ViewHandler.java
@@ -9,22 +9,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.*;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 public class ViewHandler {
-
-    private static File sort_directory;
-    private static JFrame window;
+    private static File      sort_directory;
     private static JTextArea errorArea;
-    private static String projectExtension = "flp";
-    private static String exportExtension = "mp3";
 
     public static void main(String[] args) {
-        window = createWindow();
+        createWindow();
     }
 
     // Create an window with options
@@ -33,9 +28,7 @@ public class ViewHandler {
         JFileChooser fc = createFileChooser();
 
         Button runBtn = new Button("Select Folder");
-        frame.add(runBtn);
-        runBtn.setPreferredSize(new Dimension(60, 40));
-        runBtn.setBounds(90 ,100,100, 40);
+        runBtn.setPreferredSize(new Dimension(100, 40));
         runBtn.addActionListener(e -> {
             if (fc.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
                 sort_directory = fc.getSelectedFile();
@@ -43,18 +36,15 @@ public class ViewHandler {
         });
 
         Button startBtn = new Button("Sort Files");
-        frame.add(startBtn);
-        startBtn.setPreferredSize(new Dimension(60, 40));
-        startBtn.setBounds(160,100,100, 40);
-
-        startBtn.addActionListener(e -> {
-            run();
-        });
+        startBtn.setPreferredSize(new Dimension(100, 40));
+        startBtn.addActionListener(e -> run());
 
         errorArea = new JTextArea("Error text");
         errorArea.setPreferredSize(new Dimension(100, 40));
-        frame.add(errorArea);
 
+        frame.getContentPane().add(BorderLayout.WEST, startBtn);
+        frame.add(BorderLayout.EAST, runBtn);
+        frame.add(BorderLayout.NORTH, errorArea);
         frame.setLocationRelativeTo(null); // Set window centered
         frame.pack(); // Automatically size the window to fit it's contents
         frame.setVisible(true);
@@ -66,44 +56,46 @@ public class ViewHandler {
         try {
             Multimap<String, File> filesMap = createMapOfFiles(sort_directory);
 
-            List<File> latestVersions = new ArrayList<>();
-            List<File> olderVersions = new ArrayList<>();
+            List<File> latestVersions       = new ArrayList<>();
+            List<File> olderVersions        = new ArrayList<>();
             List<File> latestExportVersions = new ArrayList<>();
-            List<File> olderExportVersions = new ArrayList<>();
+            List<File> olderExportVersions  = new ArrayList<>();
 
             // For every array of files for a given key
             for (String key : filesMap.keySet()) {
-                List<File> files = filesMap
-                    .get(key)
-                    .stream()
-                    .sorted(Comparator.comparing(File::lastModified))
-                    .collect(Collectors.toList());
 
-                files.stream()
-                    .filter(file -> FilenameUtils.getExtension(file.getName()).equals("flp"))
-                    .findFirst()
-                    .map(latestVersions::add);
+                List<File> files = new ArrayList<>(filesMap.get(key));
+                files.sort(Comparator.comparing(File::lastModified));
 
-                files.stream()
-                    .filter(file -> !FilenameUtils.getExtension(file.getName()).equals("flp"))
-                    .findFirst()
-                    .map(latestExportVersions::add);
+                boolean foundLatVer = false;
+                boolean foundLatExp = false;
 
-                files.stream()
-                    .filter(file -> FilenameUtils.getExtension(file.getName()).equals("flp"))
-                    .skip(1)
-                    .forEach(olderVersions::add);
-
-                files.stream()
-                    .filter(file -> !FilenameUtils.getExtension(file.getName()).equals("flp"))
-                    .skip(1)
-                    .map(olderExportVersions::add);
+                for (File file : files) {
+                    if (FilenameUtils.getExtension(file.getName()).equals("flp")) {
+                        if (!foundLatVer) {
+                            latestVersions.add(file);
+                            foundLatVer = true;
+                        } else {
+                            olderVersions.add(file);
+                        }
+                    } else {
+                        if (!foundLatExp) {
+                            latestExportVersions.add(file);
+                            foundLatExp = true;
+                        } else {
+                            olderExportVersions.add(file);
+                        }
+                    }
+                }
             }
 
+            System.out.println("Files sorted");
+
+            /*
             moveFilesToFolder(latestVersions, "Project Files");
             moveFilesToFolder(olderVersions, "Project Files (Old)");
             moveFilesToFolder(latestExportVersions, "Exports");
-            moveFilesToFolder(olderExportVersions, "Exports (Old)");
+            moveFilesToFolder(olderExportVersions, "Exports (Old)");*/
 
             System.out.println("Success!");
         } catch (Exception e) {
@@ -130,6 +122,7 @@ public class ViewHandler {
     private static Multimap<String, File> createMapOfFiles(File folder, Multimap<String, File> map) throws Exception {
         if (folder.exists()) {
             File[] allFiles = folder.listFiles();
+            assert allFiles != null;
             for (File file : allFiles) {
                 if (file.isDirectory()) {
                     createMapOfFiles(file, map);
@@ -142,7 +135,7 @@ public class ViewHandler {
         } else throw new Exception("Tried to sort an directory that no longer exist");
     }
 
-    public static String normaliseFileName(String name) {
+    static String normaliseFileName(String name) {
         return name
                 .replaceAll("\\s*\\([^)]*\\)\\s*", "")
                 .replaceAll("_.*", "")
