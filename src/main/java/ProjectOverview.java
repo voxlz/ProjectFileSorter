9e14a9758932a7d5ff940f3cc1669f1d3a9bdc67
Torben Nordtorp <torben.nordtorp@icloud.com>
2020-01-08 16:27:20 +0100

    Added play pause toggle
    Signed-off-by: Torben Nordtorp <torben.nordtorp@icloud.com>

--- src/main/java/ProjectOverview.java
+++ src/main/java/ProjectOverview.java
@@ -6,15 +6,16 @@ import javax.swing.*;
 import javax.swing.table.TableColumn;
 import java.io.File;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 
 public class ProjectOverview {
     public  JPanel      panel;
     private JTable      tblProjects;
-    private JButton     btnOpen;
+    MediaPlayer mediaPlayer;
     private JButton     btnOption;
     private JScrollPane scrollPane;
+    boolean playing = false;
+    private JButton btnTogglePlay;
 
     ProjectOverview(List<Project> projects) {
         ProjectTableModel model = new ProjectTableModel(projects);
@@ -25,25 +26,30 @@ public class ProjectOverview {
         column3.setCellEditor(new DefaultCellEditor(new JComboBox<>(ProjectRating.values())));
 
         new JFXPanel(); // Prepares the thing or whatnot
-        AtomicReference<MediaPlayer> mediaPlayer = new AtomicReference<>(new MediaPlayer(new Media("")));
 
         btnOption.addActionListener(e -> {
-            System.out.println("hi");
         });
 
-        btnOpen.addActionListener(e -> {
-            new JFXPanel(); // Prepares the thing or whatnot
+        btnTogglePlay.addActionListener(e -> {
+            if (playing) {
+                System.out.println("pause");
+                mediaPlayer.pause();
+                playing = false;
+                btnTogglePlay.setText("Play Selected");
+            } else {
+                System.out.println("play");
 
-            System.out.println("bye");
-            int  row          = tblProjects.getSelectedRow();
-            File latestExport = model.getProjectAt(row).latestExport;
+                int  row          = tblProjects.getSelectedRow();
+                File latestExport = model.getProjectAt(row).latestExport;
 
-            if (latestExport != null) {
-                Media song = new Media(latestExport.toURI().toString());
-                mediaPlayer.set(new MediaPlayer(song));
-                mediaPlayer.get().play();
+                if (latestExport != null) {
+                    Media song = new Media(latestExport.toURI().toString());
+                    mediaPlayer = new MediaPlayer(song);
+                    mediaPlayer.play();
+                }
+                playing = true;
+                btnTogglePlay.setText("Pause");
             }
         });
     }
-
 }
